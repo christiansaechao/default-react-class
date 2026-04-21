6bf051ba6199c4f578291422dabb44344595a170
Chris <christiansaechao23@gmail.com>
2026-04-21 12:59:15 -0700

    first day

--- morning/week-1/src/App.jsx
+++ morning/week-1/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+// component
 function App() {
-  const [count, setCount] = useState(0)
+  // javascript 
+  const [count, setCount] = useState(0);
 
+  function sum(a, b) {
+    return a + b;
+  }
+  
+  // html 
   return (
     <>
       <div>
@@ -29,7 +36,10 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
+
+
+
 }
 
-export default App
+export default App;
